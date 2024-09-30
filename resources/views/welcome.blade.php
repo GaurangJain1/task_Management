5e5c3b08e682281ad078501320de9957e222a018
GaurangJain1 <142742459+GaurangJain1@users.noreply.github.com>
2024-09-30 10:39:08 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-      <x-layout>
+  <x-layout>
     <x-slot:heading>
         HOME PAGE FOR ADMIN
     </x-slot:heading>
@@ -8,22 +8,8 @@
     <h2>Table Task is Displayed</h2>
     <p id="respanel"></p>
     <br>
-    <!-- Modal -->
-    <div id="descModal" class="modal fade" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Full Description</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body" style="word-wrap: break-word;">
-                    <p id="full-description"></p>
-                </div>
-            </div>
-        </div>
-    </div>
+    
+    
     <div id="actionModal" class="modal fade-xl" tabindex="-1" role="dialog" >
       
                       <div class="modal-dialog" role="document">
@@ -59,56 +45,24 @@
                                 @endif
                            
                         @endforeach --}}
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Sr.#</th>
-                    <th scope="col">Task Name
-                        <span class="float-right">
-                            <i class="fa fa-arrow-up"></i>
-                            <i class="fa fa-arrow-down"></i>
-                        </span> 
-                    </th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Priority</th>
-                    <th scope="col">Due Date</th>
-                    <th scope="col">Assigned to</th>
-                    <th scope="col">Task Start Date</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Last Updated On</th>
-                    <th scope="col">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach($data as $task)
-                        <tr>
-                            <th scope="row">{{$task->task_id}}</th>
-                            <td>{{ $task->task_name}}</td>
+            <div id="task-table">
+                 
+            </div>
 
-                            <td style="cursor: pointer;"><button type="button" data-description="{{ $task->task_description }}" class="description"  data-bs-toggle="modal" data-bs-target="#descModal">{{Str::limit($task->task_description,10)}}</button></td>
-                            <td>{{$task->priority}}</td>
-                            <td >{{ Carbon\Carbon::parse($task->deadline)->format('m/d/Y h:i A') }}</td> 
-                            
-                              @foreach($task->users as $user)
-                                <td>{{$user->name}}</td>                           
-                              @endforeach
-                            
-                            {{-- <td>{{$task->users}}</td> --}}
-                            <td >{{ Carbon\Carbon::parse($task->created_at)->format('m/d/Y h:i A') }}</td>
-                            <td >{{$task->current_status}}</td>
-                            <td style="opacity: 0.5;">Not Started</td>
-                            <td >{{ Carbon\Carbon::parse($task->updated_at)->format('m/d/Y h:i A') }}</td>
-                            {{-- <td >{{$task->deadline}}</td> --}}
 
-                             @foreach($task->comments as $user)
-                                                                        
-                            @endforeach  
-                            {{-- hover:bg-gray-700 hover:text-white --}}
-                            <td><button type="button" class="id" data-id="{{$task->task_id}}" data-bs-toggle="modal" data-bs-target="#actionModal">EDIT</button></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-              </table> 
+              {{-- <nav aria-label="Page navigation example" style="margin-left: auto;">
+                <ul class="pagination">
+                  <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+              </nav> --}}
+              
+                {{-- {!!$tasks->links()!!} --}}
+                {{-- {{ $tasks->links() }} --}}
+              
             {{-- <table class="table">
                 <thead>
                   <tr>
